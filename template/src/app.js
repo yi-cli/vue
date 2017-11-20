f58e6018a14e9a6fea628c3c6d5851d2a4a5f2d3
余翼 <yuyi@weidian.com>
2017-11-20 18:51:49 +0800

    [chore] add axios

--- template/src/app.js
+++ template/src/app.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import router from './router'
 import App from './App.vue'
-// import axios from './api/api.js'
-// Vue.use(axios)
+import axios from './api/api.js'
+Vue.use(axios)
 import store from './store'
 import './assets/css/base.css'
 
