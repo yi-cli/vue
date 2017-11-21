4c9192730a525808b348138f86b44d6b2ab73835
余翼 <yuyi@weidian.com>
2017-11-21 17:37:51 +0800

    [chore] meat config

--- template/src/components/HelloWorld.vue
+++ template/src/components/HelloWorld.vue
@@ -33,7 +33,7 @@ export default {
   }
 }
 </script>
-<style {{#sass}} lang="scss"{{/sass} {{#less}} lang="less"{{/less} scoped>
+<style {{#sass}} lang="scss"{{/sass}} {{#less}} lang="less"{{/less}} scoped>
 .hello {
   padding-top: 80px;
   text-align: center;
