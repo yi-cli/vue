9bab3093a5128ea9da49c4baa373c7b9fc8cb989
余翼 <yuyi@weidian.com>
2017-11-21 09:31:01 +0800

    [chore] change eslint config

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -1,8 +1,21 @@
 module.exports = {
-  "extends": [
+  parserOptions: {
+    // ECMAScript 版本
+    ecmaVersion: 2017,
+    // 支持 ECMAScript 模块
+    sourceType: 'module',
+    // 对象，表示你想使用的额外的语言特性:
+    ecmaFeatures: {
+      // 启用对实验性的 object rest/spread properties 的支持
+      experimentalObjectRestSpread: true,
+      jsx: true,
+      modules: true
+    }
+  },
+  extends: [
     "eslint-config-yi/vue",
   ],
-  "rules": {
+  rules: {
     //Your own configuration for cover my settings
   }
 }
